b19ce4ca991d7e3e5b17c856c9c796c700fb5f64
Firose Hussain <github@firumon.com>
2019-03-29 17:13:47 +0530

    Article page added

---
+++ MPA0545450/add_new_fiscal_year_on_software_2019-2020.blade.php
@@ -0,0 +1,59 @@
+@extends("pa.articles.layout")
+@section('content')
+
+    <div class="page_contents">
+        <div class="page pa" style="background-color: #EBEBEB">
+            <div class="container">
+                <br><br><br><br><br><br><br>
+                <h1><b><u>
+                            <center>How to add new fiscal year on software (2019-2020)</center>
+                        </u></b></h1>
+                <ul>
+                    <li>A fiscal year (FY) is a period that a company or government uses for accounting purposes and
+                        preparing financial statements.
+                    </li>
+                    <li>Follow the below steps It provides options to adjust duration of a fiscal year.</li>
+                </ul>
+                <h2>&nbsp;</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/PT4Go9wiqok" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <h2><u>Step by Step Instruction</u></h2>
+                <h3>Step 1</h3>
+                <div><i>Go to Tools >Settings > Fiscal year</i></div>
+                <img src="https://raw.githubusercontent.com/MilestoneInnovativeTechnologies/mit-public-articles/master/MPA0545450/001.jpg"
+                     width="712" height="463">
+
+                <h3>Step 2</h3>
+                <div><i>Select new option.</i></div>
+                <img src="https://raw.githubusercontent.com/MilestoneInnovativeTechnologies/mit-public-articles/master/MPA0545450/002.jpg"
+                     width="588" height="368">
+
+                <h3>Step 3</h3>
+                <div><i>Enter the new fiscal year details as shown below.</i></div>
+                <img src="https://raw.githubusercontent.com/MilestoneInnovativeTechnologies/mit-public-articles/master/MPA0545450/003.jpg"
+                     width="627" height="444">
+
+                <h3>Step 4</h3>
+                <div><i>Save and close the application.</i></div>
+                <img src="https://raw.githubusercontent.com/MilestoneInnovativeTechnologies/mit-public-articles/master/MPA0545450/004.jpg"
+                     width="810" height="500">
+
+                <h3>Step 5</h3>
+                <div><i>Reopen the software.</i></div>
+
+                <h3>Step 6</h3>
+                <div><i>Now while opening any window on software, a browse window with all fiscal year appears as shown
+                        below. You can choose the required year.</i></div>
+                <img src="https://raw.githubusercontent.com/MilestoneInnovativeTechnologies/mit-public-articles/master/MPA0545450/005.jpg"
+                     width="1036" height="533">
+            </div>
+        </div>
+    </div>
+@endsection
+@push('js')
+    <script type="text/javascript">
+        function login() {
+            $('#loginModal').modal('show');
+        }
+    </script>
+
+@endpush
